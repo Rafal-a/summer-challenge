3152a257a081f6c2e54b4da27da7309521485071
Rafal A <rafal.akeel99@gmail.com>
2021-08-11 18:32:24 +0300

    initialcommit

---
+++ app/src/main/java/com/thechance/summerchallenge/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.thechance.summerchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+    }
+
+
